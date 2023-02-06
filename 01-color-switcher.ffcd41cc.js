c4a0733e46f29f88bc8f41034f75674b942b6371
katya-savytska <108482673+katya-savytska@users.noreply.github.com>
2023-02-06 07:35:33 +0000

    Deploying to gh-pages from @ katya-savytska/goit-js-hw-09@8271cab6004906b1b8944798a4a98e886691a0ae 🚀

---
+++ 01-color-switcher.ffcd41cc.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){intervalId=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(intervalId),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.ffcd41cc.js.map
