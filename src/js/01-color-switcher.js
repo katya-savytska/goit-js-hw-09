52edb48c7d93e63b6be5fbd92199b5e04c545737
Савицька Катерина <savytskaa.kate@gmail.com>
2023-02-06 09:40:38 +0200

    HW 09

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,18 +5,18 @@ startBtn.addEventListener('click', changeBackgroundColor);
 stopBtn.addEventListener('click', stopChangingColor);
 
 
-function changeBackgroundColor() {
+function changeBackgroundColor(){
    intervalId = setInterval(()=>{
     document.body.style.backgroundColor = getRandomHexColor();
    }, 1000);
    startBtn.disabled = true;
 }
 
-function getRandomHexColor() {
+function getRandomHexColor(){
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
 
-  function stopChangingColor() {
+  function stopChangingColor(){
     clearInterval(intervalId);
     startBtn.disabled = false;
   }
